7b34707179112ffa7426c4c1a11f479817992f91
Muhamad Fauzan <47935196+fauzannmw@users.noreply.github.com>
2021-07-02 14:44:24 +0700

    +accordion

--- src/component/Faq.js
+++ src/component/Faq.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import { Container, Jumbotron, Row, Col, Card } from "react-bootstrap";
+import "./Faq.scss";
+// import "./Contoh.css";
 
 const Faq = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="faq">
+      <Container>
+        <Row className="justify-content-md-center">
+          <h1>B Y F E S T</h1>
+        </Row>
+        <Row className="justify-content-md-center">
+          <h5>J U D G E S</h5>
+        </Row>
+        <div className="accordionMenu">
+          <input type="radio" name="trg1" id="acc1" checked="checked" />
+          <label for="acc1">Section 1</label>
+          <div className="content">
+            <div className="inner">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </div>
+          </div>
+          <input type="radio" name="trg1" id="acc2" />
+          <label for="acc2">Section 2</label>
+          <div className="content">
+            <div className="inner">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </div>
+          </div>
         </div>
-    )
-}
+      </Container>
+    </div>
+  );
+};
 
-export default Faq
+export default Faq;
