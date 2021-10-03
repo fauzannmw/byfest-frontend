ba7e904556642ca755c498d8c6e052d6cc0fa2c8
Muhamad Fauzan <fauzanwahyudi0@gmail.com>
2021-10-03 23:01:50 +0700

    Update Sponsor.js

--- src/component/Homepage/Sponsor.js
+++ src/component/Homepage/Sponsor.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
 // import img1 from "../../assets/image/sponsor_icon_1.png";
+import post from "../../api/post";
 import "./Sponsor.scss";
 
 const Sponsor = () => {
+  const [sponsorships, setSponsorhip] = useState([]);
+
+  useEffect(async () => {
+    await post.get(`sponsor`).then(async (res) => {
+      setSponsor(res.data);
+    });
+  }, []);
+
   return (
     <div className="sponsor">
       <Container>
@@ -14,24 +23,11 @@ const Sponsor = () => {
           <b>
             <h2>Coming Soon</h2>
           </b>
-          {/* <Col xs={6} md={4} className="text-center">
-            <img src={img1} alt="" />
-          </Col>
-          <Col xs={6} md={4} className="text-center">
-            <img src={img1} alt="" />
-          </Col>
-          <Col xs={6} md={4} className="text-center">
-            <img src={img1} alt="" />
-          </Col>
-          <Col xs={6} md={4} className="text-center">
-            <img src={img1} alt="" />
-          </Col>
-          <Col xs={6} md={4} className="text-center">
-            <img src={img1} alt="" />
-          </Col>
-          <Col xs={6} md={4} className="text-center">
-            <img src={img1} alt="" />
-          </Col> */}
+          {sponsorships.map((sponsorship) => (
+            <Col xs={6} md={4} className="text-center">
+              <img src={sponsorship.Logo} alt="" />
+            </Col>
+          ))}
         </Row>
       </Container>
     </div>
