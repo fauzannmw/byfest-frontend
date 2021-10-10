82a0cf285c0090a0e8f9c09632c7b0e3c0844e78
Muhamad Fauzan <fauzanwahyudi0@gmail.com>
2021-10-10 09:34:08 +0700

    update description + faq json

--- src/component/Homepage/Description.js
+++ src/component/Homepage/Description.js
@@ -17,26 +17,46 @@ const Description = () => {
             <Slide right>
               <h1>APA ITU BRAWIJAYA FILM FESTIVAL</h1>
               <p>
-                Brawijaya Film Festival adalah acara kegiatan eksibisi dan
+                {/* Brawijaya Film Festival adalah acara kegiatan eksibisi dan
                 apresiasi film yang diproduksi oleh sineas tanah air. Rangkaian
                 kegiatan acara mencakup pemutaran film panjang lawas, film pendek
                 lokal, forum komunitas, dan talkshow. Brawijaya Film Festival yang
                 digagas oleh Unit Kegiatan Mahasiswa Nol Derajat Universitas
-                Brawijaya ini merupakan kegiatan festival film nasional.
+                Brawijaya ini merupakan kegiatan festival film nasional. */}
+                Brawijaya Film Festival atau BYFEST merupakan festival film
+                tahunan yang digagas oleh Unit Kegiatan Mahasiswa (UKM) Nol
+                Derajat Film Universitas Brawijaya. Kegiatan eksibisi ini
+                dilakukan untuk mengapresiasi film yang diproduksi oleh sineas
+                tanah air. Sebelumnya, BYFEST memiliki nama Brawijaya Movie Day
+                atau BYME, kemudian berganti nama menjadi BYFEST, ini karena
+                kami ingin mengusung konsep baru dan menjangkau target yang
+                lebih luas. Berangkat dari BYME yang merupakan festival non
+                kompetisi sehingga lebih fokus pada eksibisinya, maka kami ingin
+                BYFEST menjadi festival kompetisi yang mana menonjolkan pada
+                apresiasinya.
               </p>
               <p>
-                Brawijaya Film Festival atau BYFEST merupakan festival film
+                {/* Brawijaya Film Festival atau BYFEST merupakan festival film
                 tahunan yang digagas oleh Unit Kegiatan Mahasiswa (UKM) Nol
                 Derajat Film Universitas Brawijaya. Acara kegiatan eksibisi ini
                 dilakukan untuk mengapresiasi film yang diproduksi oleh sineas
                 tanah air. Rangkaian acara Brawijaya Film Festival mencakup
-                pemutaran film panjang lawas, program kompetisi film pendek, forum
-                komunitas, dan talkshow. Selain itu, BYFEST 2021 juga mengadakan
-                Open Submission Film Pendek. Program kompetisi film pendek
-                Brawijaya Film Festival diadakan untuk memberi wadah bagi para
-                pembuat film lokal. Adanya program kompetisi ini diharapkan dapat
-                memberi semangat dan apresiasi bagi para kreator film agar terus
-                berkarya dan berkontribusi untuk perfilman Indonesia.
+                pemutaran film panjang lawas, program kompetisi film pendek,
+                forum komunitas, dan talkshow. Selain itu, BYFEST 2021 juga
+                mengadakan Open Submission Film Pendek. Program kompetisi film
+                pendek Brawijaya Film Festival diadakan untuk memberi wadah bagi
+                para pembuat film lokal. Adanya program kompetisi ini diharapkan
+                dapat memberi semangat dan apresiasi bagi para kreator film agar
+                terus berkarya dan berkontribusi untuk perfilman Indonesia. */}
+                Rangkaian acara Brawijaya Film Festival sendiri mencakup
+                pemutaran film panjang lawas, program kompetisi film pendek,
+                forum komunitas, talkshow, dan Awarding Night. Selain itu,
+                BYFEST 2021 juga mengadakan Open Submission Film Pendek. Program
+                kompetisi film pendek Brawijaya Film Festival diadakan untuk
+                memberi wadah bagi para pembuat film lokal. Adanya program
+                kompetisi ini diharapkan dapat memberi semangat dan apresiasi
+                bagi para kreator film agar terus berkarya dan berkontribusi
+                untuk perfilman Indonesia.
               </p>
             </Slide>
           </div>
--- src/component/Homepage/Faq.js
+++ src/component/Homepage/Faq.js
@@ -13,29 +13,38 @@ const Faq = () => {
   const faqs = [
     {
       summary: "Apa itu Brawijaya Film Festival?",
-      details:
-        "Brawijaya Film Festival atau BYFEST merupakan festival film tahunan yang digagas oleh Unit Kegiatan Mahasiswa (UKM) Nol Derajat Film Universitas Brawijaya. Acara kegiatan eksibisi ini dilakukan untuk mengapresiasi film yang diproduksi oleh sineas tanah air. Rangkaian acara Brawijaya Film Festival mencakup pemutaran film panjang lawas, program kompetisi film pendek, forum komunitas, dan talkshow. Selain itu, BYFEST 2021 juga mengadakan Open Submission Film Pendek. Program kompetisi film pendek Brawijaya Film Festival diadakan untuk memberi wadah bagi para pembuat film lokal. Adanya program kompetisi ini diharapkan dapat memberi semangat dan apresiasi bagi para kreator film agar terus berkarya dan berkontribusi untuk perfilman Indonesia.",
+      details: `Brawijaya Film Festival atau BYFEST merupakan festival film tahunan yang digagas oleh Unit Kegiatan Mahasiswa (UKM) 
+        Nol Derajat Film Universitas Brawijaya. Acara kegiatan eksibisi ini dilakukan untuk mengapresiasi film yang diproduksi 
+        oleh sineas tanah air. Rangkaian acara Brawijaya Film Festival mencakup pemutaran film panjang lawas, program kompetisi 
+        film pendek, forum komunitas, dan talkshow. Selain itu, BYFEST 2021 juga mengadakan Open Submission Film Pendek. 
+        Program kompetisi film pendek Brawijaya Film Festival diadakan untuk memberi wadah bagi para pembuat film lokal. Adanya program 
+        kompetisi ini diharapkan dapat memberi semangat dan apresiasi bagi para kreator film agar terus berkarya dan berkontribusi 
+        untuk perfilman Indonesia.`,
     },
     {
       summary: "Apa Tema BYFEST tahun ini?",
-      details:
-        "Tahun ini BYFEST mengusung tema “C’est La Vie”, yang merupakan ungkapan dari Bahasa Perancis yang memiliki arti ‘itulah kehidupan’ atau ‘inilah kehidupan’.  Ungkapan tersebut mampu mengekspresikan perasaan, negatif maupun positif, manusia dalam menjalani kehidupan. Dengan memberi karakteristik “c’est la vie” kepada film-film yang akan ditayangkan, kami mengajak penonton untuk merenungkan kehidupan dan menimbulkan perasaan yang kuat dari penonton. Selain itu, film tersebut juga dapat memberi wawasan baru mengenai isu kehidupan yang jarang bahkan tidak dialami oleh penonton. Contoh film yang memiliki karakteristik “c’est la vie” adalah film yang menceritakan cinta, keluarga, perjuangan, pertikaian, ideologi, kebangkitan, perubahan, alam, masyarakat, teknologi, dan aspek-aspek kehidupan lainnya  yang dapat terjadi di kisah penonton.",
+      details: `Tahun ini BYFEST mengusung tema “C’est La Vie”, yang merupakan ungkapan dari Bahasa Perancis yang memiliki arti 
+        ‘itulah kehidupan’ atau ‘inilah kehidupan’.  Ungkapan tersebut mampu mengekspresikan perasaan, negatif maupun positif, 
+        manusia dalam menjalani kehidupan. Dengan memberi karakteristik “c’est la vie” kepada film-film yang akan ditayangkan, 
+        kami mengajak penonton untuk merenungkan kehidupan dan menimbulkan perasaan yang kuat dari penonton. Selain itu, 
+        film tersebut juga dapat memberi wawasan baru mengenai isu kehidupan yang jarang bahkan tidak dialami oleh penonton. 
+        Contoh film yang memiliki karakteristik “c’est la vie” adalah film yang menceritakan cinta, keluarga, perjuangan, pertikaian, 
+        ideologi, kebangkitan, perubahan, alam, masyarakat, teknologi, dan aspek-aspek kehidupan lainnya  yang dapat terjadi di kisah penonton.`,
     },
     {
       summary:
         "Apakah film yang disubmit akan ditayangkan di YouTube atau platform lain?",
-      details:
-        "Nantinya film tersebut akan dimasukkan ke dalam platform OTT yang telah bekerjasama dengan BYFEST secara live streaming dan hanya akan tersedia selama acara BYFEST berlangsung.",
+      details: `Nantinya film tersebut akan dimasukkan ke dalam platform OTT yang telah bekerjasama dengan BYFEST secara live streaming 
+        dan hanya akan tersedia selama acara BYFEST berlangsung.`,
     },
     {
       summary: "Apakah acara berbayar atau tidak?",
-      details:
-        "Acara yang kami selenggarakan adalah GRATIS tetapi terdapat donasi minimal Rp10.000,00 untuk perkembangan film Indonesia.",
+      details: `Acara yang kami selenggarakan adalah berbayar (satu tiket sudah termasuk 3 hari rangkaian acara), untuk detail pemesanan 
+      dapat dilihat melalui loket.com/…. atau dengan meng-klik Get Ticket pada homepage website Brawijaya Film Festival`,
     },
     {
       summary: "Achievement apa yang akan didapatkan pemenang?",
-      details:
-        "Achievement yang akan didapat oleh pemenang adalah uang tunai dengan total Rp2.500.000,00, plakat, dan sertifikat.",
+      details: `Achievement yang akan didapat oleh pemenang adalah uang tunai dengan total Rp2.500.000,00, plakat, dan sertifikat.`,
     },
   ];
   return (
