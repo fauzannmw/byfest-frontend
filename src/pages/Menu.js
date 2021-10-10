931aca60ea11ce356975da86d6d15757e1c877c2
argafrl <arga_muhammad@yahoo.co.id>
2021-10-10 19:21:59 +0700

    add tooltips for day button and fix table ui in menu

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,9 +1,23 @@
 import { Link } from 'react-router-dom';
-import { Table } from 'react-bootstrap';
+import { OverlayTrigger, Table, Tooltip } from 'react-bootstrap';
 import './Menu.scss'
 import { useState } from 'react';
 
 const Menu = () => {
+  const buttonDay = [
+    {
+      day: "1",
+      toolTip: "Jumat, 29 Oktober 2021",
+    },
+    {
+      day: "2",
+      toolTip: "Sabtu, 30 Oktober 2021",
+    },
+    {
+      day: "3",
+      toolTip: "Minggu, 31 Oktober 2021",
+    }
+  ]
   const [day, setDay] = useState('1');
   return (
     <div className="menu">
@@ -11,78 +25,83 @@ const Menu = () => {
         <div className="container">
 
           <div className="button-day-wrapper">
-            <button
-              className="button-day"
-              type="submit"
-              onClick={() => setDay('1')}
-            >Day - 1</button>
-            <button
-              className="button-day"
-              type="submit"
-              onClick={() => setDay('2')}
-            >Day - 2</button>
-            <button
-              className="button-day"
-              type="submit"
-              onClick={() => setDay('3')}
-            >Day - 3</button>
+            {buttonDay.map((btn) => {
+              return (
+                <OverlayTrigger
+                  key={btn.day}
+                  placement="top"
+                  overlay={
+                    <Tooltip id="button-tooltip">
+                      {btn.toolTip}
+                    </Tooltip>
+                  }
+                  delay={{ show: 50, hide: 50 }}
+                >
+                  <button
+                    className="button-day"
+                    type="submit"
+                    onClick={() => setDay(btn.day)}
+                  >Day - {btn.day}</button>
+                </OverlayTrigger>
+              );
+            })}
           </div>
 
           <div className="table-wrapper">
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Event</th>
-                <th>Jam</th>
-              </tr>
-            </thead>
-            { day=='1' && <tbody>
-              <tr>
-                <td>Opening</td>
-                <td>09.00 WIB</td>
-              </tr>
-              <tr>
-                <td>Webinar</td>
-                <td>13.00 WIB</td>
-              </tr>
-              <tr>
-                <td>Program Film Pendek Dokumenter Kompetitif</td>
-                <td>14.15 WIB</td>
-              </tr>
-              <tr>
-                <td>Program Film Panjang 1</td>
-                <td>19.15 WIB</td>
-              </tr>
-            </tbody> }
-            { day=='2' && <tbody>
-              <tr>
-                <td>Program Film Pendek Kompetitif 1</td>
-                <td>10.00 WIB</td>
-              </tr>
-              <tr>
-                <td>Program Film Pendek Kompetitif 2</td>
-                <td>13.00 WIB</td>
-              </tr>
-              <tr>
-                <td>Forkom</td>
-                <td>19.00 WIB</td>
-              </tr>
-            </tbody> }
-            { day=='3' && <tbody>
-              <tr>
-                <td>Program Film Pendek Non Kompetitif</td>
-                <td>10.00 WIB</td>
-              </tr>
-              <tr>
-                <td>Program Film Panjang 2</td>
-                <td>13.00 WIB</td>
-              </tr>
-              <tr>
-                <td>Awarding</td>
-                <td>19.05 WIB</td>
-              </tr>
-            </tbody> }
-          </Table>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Event</th>
+                  <th>Jam</th>
+                </tr>
+              </thead>
+              { day=='1' && <tbody>
+                <tr>
+                  <td>Opening</td>
+                  <td>09.00 WIB</td>
+                </tr>
+                <tr>
+                  <td>Webinar</td>
+                  <td>13.00 WIB</td>
+                </tr>
+                <tr>
+                  <td>Program Film Pendek Dokumenter Kompetitif</td>
+                  <td>14.15 WIB</td>
+                </tr>
+                <tr>
+                  <td>Program Film Panjang 1</td>
+                  <td>19.15 WIB</td>
+                </tr>
+              </tbody> }
+              { day=='2' && <tbody>
+                <tr>
+                  <td>Program Film Pendek Kompetitif 1</td>
+                  <td>10.00 WIB</td>
+                </tr>
+                <tr>
+                  <td>Program Film Pendek Kompetitif 2</td>
+                  <td>13.00 WIB</td>
+                </tr>
+                <tr>
+                  <td>Forkom</td>
+                  <td>19.00 WIB</td>
+                </tr>
+              </tbody> }
+              { day=='3' && <tbody>
+                <tr>
+                  <td>Program Film Pendek Non Kompetitif</td>
+                  <td>10.00 WIB</td>
+                </tr>
+                <tr>
+                  <td>Program Film Panjang 2</td>
+                  <td>13.00 WIB</td>
+                </tr>
+                <tr>
+                  <td>Awarding</td>
+                  <td>19.05 WIB</td>
+                </tr>
+              </tbody> }
+            </Table>
           </div>
 
           <div className="button-visit-wrapper">
